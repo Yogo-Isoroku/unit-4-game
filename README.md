4f11b6b90c317c76d13bd315a8fd29835713d0b1
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-08-27 09:59:06 -0700

    updated Readme. reformated all files. Removed unneded comments

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # [Crystal Madness](https://funkaj.github.io/Crystal-Madness/)
-A matching game made for UCSD Bootcamp. 
-User is given a random target number (between 19-120) to meet not exceed. The four crystals are given random numbers(between 1-12) with no duplicates. User must click on crystals to update Player Total and remember what the number under each crystal is. If they meet the target they win, if they go over they lose. Win/Lose counter updates and A new Target Number/Crystals are selected. Player continues for as long as they like. 
+
+A matching game made for UCSD Bootcamp.
+User is given a random target number (between 19-120) to meet not exceed. The four crystals are given random numbers(between 1-12) with no duplicates. User must click on crystals to update Player Total and remember what the number under each crystal is. If they meet the target they win, if they go over they lose. Win/Lose counter updates and A new Target Number/Crystals are selected. Player continues for as long as they like.
 
 ![Demo](/assets/images/crystalGame.gif)
 
 ## How to Use
-* Click link [here](https://funkaj.github.io/Crystal-Madness/)
-* Click on the screen to dismiss instructions.
-* Find your target number
-* Start clicking on crystals and try and remember their value.
-* Attempt to meet the target number without exceeding. 
+
+- Click link [here](https://funkaj.github.io/Crystal-Madness/)
+- Click on the screen to dismiss instructions.
+- Find your target number
+- Start clicking on crystals and try and remember their value.
+- Attempt to meet the target number without exceeding.
 
 ## Built With
-* [HTML]
-* [CSS]
-* [Bootstrap](https://getbootstrap.com/) - Bootstrap
-* [jQuery](http://jquery.com/) - jQuery
+
+- HTML
+- CSS
+- [Bootstrap](https://getbootstrap.com/) - Bootstrap
+- [jQuery](http://jquery.com/) - jQuery
 
 ## Authors
-* **Adam Funk** 
-* [Github](https://github.com/funkaj)
-* [LinkedIn](https://www.linkedin.com/in/adam-funk-5831b8152/)
+
+- **Adam Funk**
+- [Github](https://github.com/funkaj)
+- [LinkedIn](https://www.linkedin.com/in/adam-funk-5831b8152/)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,155 +1,162 @@
 //instructions Game starts on click
-$(document).ready(function () {
-    $('.btn').hide();
+$(document).ready(function() {
+	$('.btn').hide();
 
-    $('#instructions').on('click', function () {
-        bgMusic.addEventListener('ended', function () {
-            this.currentTime = 0;
-            this.play();
-        }, 
-        false);
-        bgMusic.play();
-        $('.btn').show();
-        $('#instructions').hide();
-    });
+	$('#instructions').on('click', function() {
+		bgMusic.addEventListener(
+			'ended',
+			function() {
+				this.currentTime = 0;
+				this.play();
+			},
+			false
+		);
+		bgMusic.play();
+		$('.btn').show();
+		$('#instructions').hide();
+	});
 
-    //choose a target number between 19 and 120
-    const bgMusic = new Audio('./assets/audio/371516__mrthenoronha__space-game-theme-loop.ogg');
-    let targetNumber = [Math.floor(19 + Math.random() * 101)];
-    let wins = 0;
-    let losses = 0;
-    let counter = 0;
+	//choose a target number between 19 and 120
+	const bgMusic = new Audio(
+		'./assets/audio/371516__mrthenoronha__space-game-theme-loop.ogg'
+	);
+	let targetNumber = [Math.floor(19 + Math.random() * 101)];
+	let wins = 0;
+	let losses = 0;
+	let counter = 0;
 
-    bgMusic.addEventListener('ended', function () {
-        this.currentTime = 0;
-        this.play();
-    }, false);
-    bgMusic.play();
+	bgMusic.addEventListener(
+		'ended',
+		function() {
+			this.currentTime = 0;
+			this.play();
+		},
+		false
+	);
+	bgMusic.play();
 
-    $("#number-to-guess").text(targetNumber);
-    $('.wins').text(wins);
-    $('.losses').text(losses);
-    $('.userTotal').text(counter);
+	$('#number-to-guess').text(targetNumber);
+	$('.wins').text(wins);
+	$('.losses').text(losses);
+	$('.userTotal').text(counter);
 
-    //pick and assign to the crystals 1-12 no dupes
-    let secretNumbers = [];
+	//pick and assign to the crystals 1-12 no dupes
+	let secretNumbers = [];
 
-    while (secretNumbers.length < 4) {
-        let crystalNumber = Math.floor(1 + Math.random() * 11);
-        if (!secretNumbers.includes(crystalNumber)) {
-            secretNumbers.push(Number(crystalNumber));
-        };
-    };
+	while (secretNumbers.length < 4) {
+		let crystalNumber = Math.floor(1 + Math.random() * 11);
+		if (!secretNumbers.includes(crystalNumber)) {
+			secretNumbers.push(Number(crystalNumber));
+		}
+	};
 
-    //pull from array to assign numbers to crystals
-    let red = secretNumbers[0];
-    let amber = secretNumbers[1];
-    let purple = secretNumbers[2];
-    let green = secretNumbers[3];
+	//pull from array to assign numbers to crystals
+	let red = secretNumbers[0];
+	let amber = secretNumbers[1];
+	let purple = secretNumbers[2];
+	let green = secretNumbers[3];
 
-    // new game function
-    function newGame() {
-        counter = 0;
-        targetNumber = [Math.floor(19 + Math.random() * 101)]; 
+	// new game function
+	function newGame() {
+		counter = 0;
+		targetNumber = [Math.floor(19 + Math.random() * 101)];
 
-        $("#number-to-guess").text(targetNumber);
-        $('.userTotal').text(counter = 0);
+		$('#number-to-guess').text(targetNumber);
+		$('.userTotal').text((counter = 0));
 
-        secretNumbers = [];
+		secretNumbers = [];
 
-        while (secretNumbers.length < 4) {
-            let crystalNumber = Math.floor(1 + Math.random() * 11);
-            if (!secretNumbers.includes(crystalNumber)) {
-                secretNumbers.push(Number(crystalNumber));
+		while (secretNumbers.length < 4) {
+			let crystalNumber = Math.floor(1 + Math.random() * 11);
+			if (!secretNumbers.includes(crystalNumber)) {
+				secretNumbers.push(Number(crystalNumber));
+			}
+		}
 
-            };
-        };
+		red = secretNumbers[0];
+		amber = secretNumbers[1];
+		purple = secretNumbers[2];
+		green = secretNumbers[3];
+	};
 
-        red = secretNumbers[0];
-        amber = secretNumbers[1];
-        purple = secretNumbers[2];
-        green = secretNumbers[3];
-    };
+	// User win
+	function userWin() {
+		wins++;
+		$('.wins').text(wins);
+		newGame();
+	};
 
-    // User win
-    function userWin() {
-        wins++;
-        $('.wins').text(wins);
-        newGame();
-    };
+	//User loss
+	function userLose() {
+		losses++;
+		$('.losses').text(losses);
+		newGame();
+	};
 
-    //User loss
-    function userLose() {
-        losses++;
-        $('.losses').text(losses);
-        newGame();
-    };
+	//attaching number to crystals and click directions
+	$('.red').on('click', function() {
+		counter = counter + red;
 
-    //attaching number to crystals and click directions
-    $('.red').on('click', function () {
-        counter = counter + red;
+		$('.userTotal').text(counter);
 
-        $('.userTotal').text(counter);
+		if (counter == targetNumber) {
+			userWin();
+		} else if (counter >= targetNumber) {
+			userLose();
+		}
+	});
 
-        if (counter == targetNumber) {
-            userWin();
-        } else if (counter >= targetNumber) {
-            userLose();
-        };
-    });
+	$('.amber').on('click', function() {
+		counter = counter + amber;
 
-    $('.amber').on('click', function () {
-        counter = counter + amber;
+		$('.userTotal').text(counter);
 
-        $('.userTotal').text(counter);
+		if (counter == targetNumber) {
+			userWin();
+		} else if (counter >= targetNumber) {
+			userLose();
+		}
+	});
 
-        if (counter == targetNumber) {
-            userWin();
-        } else if (counter >= targetNumber) {
-            userLose();
-        };
-    });
+	$('.purple').on('click', function() {
+		counter = counter + purple;
 
-    $('.purple').on('click', function () {
-        counter = counter + purple;
+		$('.userTotal').text(counter);
 
-        $('.userTotal').text(counter);
+		if (counter == targetNumber) {
+			userWin();
+		} else if (counter >= targetNumber) {
+			userLose();
+		}
+	});
 
-        if (counter == targetNumber) {
-            userWin();
-        } else if (counter >= targetNumber) {
-            userLose();
-        };
-    });
+	$('.green').on('click', function() {
+		counter = counter + green;
+		$('.userTotal').text(counter);
+		if (counter == targetNumber) {
+			userWin();
+		} else if (counter >= targetNumber) {
+			userLose();
+		}
+	});
 
-    $('.green').on('click', function () {
-        counter = counter + green;
-        $('.userTotal').text(counter);
-        if (counter == targetNumber) {
-            userWin();
-        } else if (counter >= targetNumber) {
-            userLose();
-        };
-    });
+	$('#forfeitGame').on('click', function() {
+		userLose();
+	});
 
-    $('#forfeitGame').on('click', function () {
-        userLose();
-    });
+	$('#restartGame').on('click', function() {
+		wins = 0;
+		losses = 0;
+		$('.losses').text(losses);
+		$('.wins').text(wins);
+		newGame();
+	});
 
-    $('#restartGame').on('click', function () {
-        wins = 0;
-        losses = 0;
-        $('.losses').text(losses);
-        $('.wins').text(wins);
-        newGame();
-    });
+	$('#playMusic').on('click', function() {
+		bgMusic.play();
+	});
 
-    $('#playMusic').on('click', function () {
-        bgMusic.play();
-    });
-
-    $('#pauseMusic').on('click', function () {
-        bgMusic.pause();
-    });
-
+	$('#pauseMusic').on('click', function() {
+		bgMusic.pause();
+	});
 });
